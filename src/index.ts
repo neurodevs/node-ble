2ca439960ff50cde6228a009870fc09faa1fed18
eric-yates <hello@ericthecurious.com>
2024-10-27 10:26:20 -0600

    patch: export modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-//exports go here
+export { default as BleScannerImpl } from './BleScanner'
+export * from './BleScanner'
+
+export { default as SpyBleScanner } from './testDoubles/SpyBleScanner'
+export * from './testDoubles/SpyBleScanner'
+
+export { default as FakeBleScanner } from './testDoubles/FakeBleScanner'
+export * from './testDoubles/FakeBleScanner'
+
+export { default as BleAdapterImpl } from './BleAdapter'
+export * from './BleAdapter'
+
+export { default as SpyBleAdapter } from './testDoubles/SpyBleAdapter'
+export * from './testDoubles/SpyBleAdapter'
+
+export { default as FakeBleAdapter } from './testDoubles/FakeBleAdapter'
+export * from './testDoubles/FakeBleAdapter'
+
+export { default as FakeNoble } from './testDoubles/FakeNoble'
+export * from './testDoubles/FakeNoble'
+
+export { default as FakePeripheral } from './testDoubles/FakePeripheral'
+export * from './testDoubles/FakePeripheral'
