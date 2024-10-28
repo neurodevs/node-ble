5334ba125abdf4a0fa2a273ccab8bbf4f07e5d3f
eric-yates <hello@ericthecurious.com>
2024-10-27 21:42:34 -0600

    patch: undo demo update

--- src/demo.ts
+++ src/demo.ts
@@ -10,8 +10,6 @@ async function main() {
     const result = await scanner.scanForName('CGX Quick-Series Headset')
     console.log('Result:\n\n', result)
 
-    await new Promise((resolve) => setTimeout(resolve, 1000000)).then(() => {})
-
     exit(0)
 }
 
