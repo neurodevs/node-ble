3c9ab8aeb0e3b0c629c9f4bf5fb607c43e62a17b
ericthecurious <hello@ericthecurious.com>
2024-12-29 12:39:01 -0700

    patch: reorder exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-// Components
-
-export { default as BleDeviceScanner } from './components/BleDeviceScanner'
-export * from './components/BleDeviceScanner'
+// BleAdapter
 
 export { default as BleDeviceAdapter } from './components/BleDeviceAdapter'
 export * from './components/BleDeviceAdapter'
 
-// Test doubles
+export { default as SpyBleAdapter } from './testDoubles/BleAdapter/SpyBleAdapter'
+export * from './testDoubles/BleAdapter/SpyBleAdapter'
 
 export { default as FakeBleAdapter } from './testDoubles/BleAdapter/FakeBleAdapter'
 export * from './testDoubles/BleAdapter/FakeBleAdapter'
 
+// BleScanner
+
+export { default as BleDeviceScanner } from './components/BleDeviceScanner'
+export * from './components/BleDeviceScanner'
+
+export { default as SpyBleScanner } from './testDoubles/BleScanner/SpyBleScanner'
+export * from './testDoubles/BleScanner/SpyBleScanner'
+
 export { default as FakeBleScanner } from './testDoubles/BleScanner/FakeBleScanner'
 export * from './testDoubles/BleScanner/FakeBleScanner'
+
+// noble
+
+export { default as SpyCharacteristic } from './testDoubles/noble/SpyCharacteristic'
+export * from './testDoubles/noble/SpyCharacteristic'
 
 export { default as FakeCharacteristic } from './testDoubles/noble/FakeCharacteristic'
 export * from './testDoubles/noble/FakeCharacteristic'
@@ -22,12 +33,3 @@ export * from './testDoubles/noble/FakeNoble'
 
 export { default as FakePeripheral } from './testDoubles/noble/FakePeripheral'
 export * from './testDoubles/noble/FakePeripheral'
-
-export { default as SpyBleAdapter } from './testDoubles/BleAdapter/SpyBleAdapter'
-export * from './testDoubles/BleAdapter/SpyBleAdapter'
-
-export { default as SpyBleScanner } from './testDoubles/BleScanner/SpyBleScanner'
-export * from './testDoubles/BleScanner/SpyBleScanner'
-
-export { default as SpyCharacteristic } from './testDoubles/noble/SpyCharacteristic'
-export * from './testDoubles/noble/SpyCharacteristic'
