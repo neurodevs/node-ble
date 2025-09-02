c9ba6fa467bff391303863e4d071562121f11f56
ericthecurious <hello@ericthecurious.com>
2025-09-01 17:13:08 -0700

    major: minor refactor to conventions and release v1

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // BleConnector
 
-export { default as BleDeviceConnector } from './components/BleDeviceConnector'
-export * from './components/BleDeviceConnector'
+export { default as BleDeviceConnector } from './modules/BleDeviceConnector'
+export * from './modules/BleDeviceConnector'
 
 export { default as FakeBleConnector } from './testDoubles/BleConnector/FakeBleConnector'
 export * from './testDoubles/BleConnector/FakeBleConnector'
 
 // BleController
 
-export { default as BleDeviceController } from './components/BleDeviceController'
-export * from './components/BleDeviceController'
+export { default as BleDeviceController } from './modules/BleDeviceController'
+export * from './modules/BleDeviceController'
 
 export { default as SpyBleController } from './testDoubles/BleController/SpyBleController'
 export * from './testDoubles/BleController/SpyBleController'
@@ -19,8 +19,8 @@ export * from './testDoubles/BleController/FakeBleController'
 
 // BleScanner
 
-export { default as BleDeviceScanner } from './components/BleDeviceScanner'
-export * from './components/BleDeviceScanner'
+export { default as BleDeviceScanner } from './modules/BleDeviceScanner'
+export * from './modules/BleDeviceScanner'
 
 export { default as SpyBleScanner } from './testDoubles/BleScanner/SpyBleScanner'
 export * from './testDoubles/BleScanner/SpyBleScanner'
@@ -30,11 +30,11 @@ export * from './testDoubles/BleScanner/FakeBleScanner'
 
 // noble
 
-export { default as SpyCharacteristic } from './testDoubles/noble/SpyCharacteristic'
-export * from './testDoubles/noble/SpyCharacteristic'
-
 export { default as FakeCharacteristic } from './testDoubles/noble/FakeCharacteristic'
 export * from './testDoubles/noble/FakeCharacteristic'
+
+export { default as SpyCharacteristic } from './testDoubles/noble/SpyCharacteristic'
+export * from './testDoubles/noble/SpyCharacteristic'
 
 export { default as FakeNoble } from './testDoubles/noble/FakeNoble'
 export * from './testDoubles/noble/FakeNoble'
